3f85164f075e585949acab55b0b0d76564dd3098
Luis Saucedo <a2019111501@estudiantes.upsa.edu.bo>
2021-01-13 19:21:07 -0400

    Cambié el saludo

---
+++ Primer_commit/Primer_commit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primer_commit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenas profes arreglé mi problema, atte Luis Saucedo");
+        }
+    }
+}
